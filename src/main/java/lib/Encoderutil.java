5f5091be3913124dcad5a88c67ba939a3460ea42
v0ncent <83373495+v0ncent@users.noreply.github.com>
2024-02-06 00:43:57 -0600

    added neo encoder linear distance conversion factor in meters for autos this year

--- src/main/java/lib/Encoderutil.java
+++ src/main/java/lib/Encoderutil.java
@@ -1,5 +1,7 @@
 package lib;
 
+import edu.wpi.first.math.util.Units;
+
 /**
  * A utility class housing some useful algorithms for working with encoders.
  */
@@ -7,6 +9,8 @@ public final class Encoderutil {
     /** 
      * Takes a REV internal encoder position and converts it to feet.
      * @param encoderPosition rev internal encoder position.
+     * @param gearRatio Drivetrain gear ratio.
+     * @param wheelDiameter diameter of driveTrain wheels.
      * @return encoder position converted to feet.
      */
     public static double neoEncoderTicks2Feet(double encoderPosition, double gearRatio, double wheelDiameter) {
@@ -20,9 +24,22 @@ public final class Encoderutil {
     /**
      * Takes a REV internal encoder position and converts it to inches.
      * @param encoderPosition rev internal encoder position.
-     * @return encoder position converted to feet.
+     * @param gearRatio Drivetrain gear ratio.
+     * @param wheelDiameter diameter of driveTrain wheels.
+     * @return encoder position converted to inches.
      */
     public static double neoEncoderTicks2Inches(double encoderPosition, double gearRatio, double wheelDiameter) {
         return encoderPosition * ((wheelDiameter*Math.PI)/gearRatio);
+    }
+
+    /**
+     * Creates a linear distance conversion factor for a neo encoder based upon gear ratio and wheel radius
+     * @param gearRatio Drivetrain gear ratio.
+     * @param wheelRadiusInches The radius of the drivetrain wheels in inches.
+     * @return A neo encoder linear distance conversion factor to be used as a conversion factor in neo encoders.
+     * @implNote THIS IS IN METERS!
+     */
+    public static double linearDistanceConversionFactor(double gearRatio, double wheelRadiusInches) {
+        return Units.inchesToMeters(1 / (gearRatio * 2 * Math.PI * Units.inchesToMeters(wheelRadiusInches) * 10));
     }
 }
