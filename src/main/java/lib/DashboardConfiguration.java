4a12d25d32335d534636a11501a80eb8f7133342
v0ncent <banksvincent5@gmail.com>
2024-04-27 16:39:13 -0500

    documentation

--- src/main/java/lib/DashboardConfiguration.java
+++ src/main/java/lib/DashboardConfiguration.java
@@ -2,7 +2,7 @@ package lib;
 
 /**
  * Provides a nice method to configure a dashboard with.
- * @implNote This interfaces method should be called via a subsystems periodic method.
+ * @implNote This interface's method should be called via a subsystems periodic method.
  * @see edu.wpi.first.wpilibj2.command.SubsystemBase
  */
 @FunctionalInterface
