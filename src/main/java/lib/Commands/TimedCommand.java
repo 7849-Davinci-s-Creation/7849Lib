fca19cd5c994b83a85e4ba28fba584b234b0a546
v0ncent <banksvincent5@gmail.com>
2024-04-26 21:41:05 -0500

    documentation

--- src/main/java/lib/Commands/TimedCommand.java
+++ src/main/java/lib/Commands/TimedCommand.java
@@ -3,17 +3,41 @@ package lib.Commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
+/**
+ * An instance of a WPILib Command that is executed based on a given time limit.
+ * @see Command
+ */
 public abstract class TimedCommand extends Command {
     private final double seconds;
 
     private double startingTime;
+
+    /**
+     * Creates an instance of a Timed WPILib Command.
+     * @param seconds Number of seconds Command should execute.
+     * @param requirements List of Subsystems command is executing upon.
+     */
     public TimedCommand(double seconds, Subsystem... requirements) {
         this.seconds = seconds;
         addRequirements(requirements);
     }
 
+    /**
+     * Initial subroutine of Command.
+     * @see Command
+     */
     public abstract void init();
+
+    /**
+     * Main body of command.
+     * @see Command
+     */
     public abstract void exec();
+
+    /**
+     * Action to take when command ends
+     * @see Command
+     */
     public abstract void end();
 
     @Override
