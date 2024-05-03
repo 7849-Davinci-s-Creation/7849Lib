41c3b0912464249792f9fd830ab0af9ed2a07d2a
v0ncent <83373495+v0ncent@users.noreply.github.com>
2024-05-03 03:50:19 -0500

    uploaded all useful code to library from season!

--- src/main/java/lib/Constants.java
+++ src/main/java/lib/Constants.java
@@ -4,8 +4,11 @@ package lib;
  * A class housing all constants of 7849Lib.
  */
 public final class Constants {
-    /**Number of inches in one foot.*/
+    /**Number of inches in one foot. */
     public static final int INCHES_IN_FEET = 12;
     /**Number of inches in a Meter. */
-    public static final double INCHES_IN_METER = 0.0254; 
+    public static final double INCHES_IN_METER = 0.0254;
+    /**Radius of Six Inch Highgrip wheels in inches. (<a href="https://www.andymark.com/products/aussie-sku-6-in-higrip-wheel-80-durometer-white">Example</a>) */
+    public static final int SIX_INCH_HIGHGRIP_WHEEL_RADIUS = 3;
+
 }
