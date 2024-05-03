3c7438077cf2db5ac1d2e717487725c36d7fc6f5
v0ncent <83373495+v0ncent@users.noreply.github.com>
2024-05-03 03:55:16 -0500

    removed @implnote cuz gradles doesnt know it

--- src/main/java/lib/DashboardConfiguration.java
+++ src/main/java/lib/DashboardConfiguration.java
@@ -2,7 +2,7 @@ package lib;
 
 /**
  * Provides a nice method to configure a dashboard with.
- * @implNote This interface's method should be called via a subsystems periodic method.
+ * (This interface's method should be called via a subsystems periodic method.)
  * @see edu.wpi.first.wpilibj2.command.SubsystemBase
  */
 @FunctionalInterface
